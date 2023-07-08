456957df31d3489e6ef2a7dc624ff68f8a8f1e20
Ben Rasmussen <bigbadbeard@tutanota.com>
2023-07-07 21:40:57 -0500

    Additions to front end

--- event-ticketing/src/App.jsx
+++ event-ticketing/src/App.jsx
@@ -1,25 +1,46 @@
 import { ethers } from 'ethers';
 import { useState } from 'react';
+import eventTicketingArtifact from './EventTicketing.json';  
+
+
+
 
 // ...existing component code...
 
-function App() {
-  // ...existing component state and effects...
+const App = () => {
+  const [maxTickets, setMaxTickets] = useState("");
+  const [eventLocation, setEventLocation] = useState("");
+  const [eventName, setEventName] = useState("");
+  const [eventTime, setEventTime] = useState("");
+  const provider = new ethers.BrowserProvider(window.ethereum);
 
-  const [seatNumber, setSeatNumber] = useState<string>('');
+  const deployContract = async () => {
+    const signer = await provider.getSigner();
+    const EventTicketingABI = eventTicketingArtifact.abi;
+    const EventTicketingBytecode = eventTicketingArtifact.bytecode;
+    
 
-  const buyTicket = async () => {
-    if (contract && seatNumber) {
-      const ticketPrice = ethers.utils.parseEther('1'); // Replace with actual ticket price
-      const transactionResponse = await contract.buyTicket(seatNumber, { value: ticketPrice });
-      await transactionResponse.wait();
-    }
+    const EventTicketingFactory = new ethers.ContractFactory(EventTicketingABI, EventTicketingBytecode, signer);
+
+    console.log(EventTicketingFactory);
+
+    const eventTicketing = await EventTicketingFactory.deploy(
+      maxTickets, 
+      eventLocation, 
+      eventName, 
+      eventTime
+    );
+
+    console.log("EventTicketing deployed to:", eventTicketing.address);
   };
 
   return (
-    <div className="App">
-      <input value={seatNumber} onChange={(e) => setSeatNumber(e.target.value)} placeholder="Seat Number" />
-      <button onClick={buyTicket}>Buy Ticket</button>
+    <div>
+      <input value={maxTickets} onChange={(e) => setMaxTickets(e.target.value)} placeholder="Max Tickets" />
+      <input value={eventLocation} onChange={(e) => setEventLocation(e.target.value)} placeholder="Event Location" />
+      <input value={eventName} onChange={(e) => setEventName(e.target.value)} placeholder="Event Name" />
+      <input value={eventTime} onChange={(e) => setEventTime(e.target.value)} placeholder="Event Time" />
+      <button onClick={deployContract}>Deploy Contract</button>
     </div>
   );
 }
