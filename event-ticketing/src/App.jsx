58808edd7992dc188972ab824717c715e830309a
Travis Richardson <tlrichar@gmail.com>
2023-07-10 10:31:20 -0400

    added linter to support BigInt, and broke date/time up

--- event-ticketing/src/App.jsx
+++ event-ticketing/src/App.jsx
@@ -1,4 +1,4 @@
-import { ethers } from 'ethers';
+import React, { ethers } from 'ethers';
 import { useState } from 'react';
 import eventTicketingArtifact from './EventTicketing.json';  
 import './App.css'; // Import the app.css file
@@ -11,24 +11,36 @@ const App = () => {
   const [maxTickets, setMaxTickets] = useState("");
   const [eventLocation, setEventLocation] = useState("");
   const [eventName, setEventName] = useState("");
+  const [eventDate, setEventDate] = useState("");
   const [eventTime, setEventTime] = useState("");
   const [contractAddress, setContractAddress] = useState("");
   const provider = new ethers.BrowserProvider(window.ethereum);
+
+  let eventDateTimeInUint256 = 0;
+
+  const convertDateTime = async () => {
+    const eventDateTime = `${eventDate} ${eventTime}`;
+    const dateObject = new Date(eventDateTime);
+    const unixTimestamp = dateObject.getTime();
+    const unixTimestampInSeconds = Math.floor(unixTimestamp / 1000);
+    const timeInUint256 = dateObject.getHours() * 3600 + dateObject.getMinutes() * 60;
+    eventDateTimeInUint256 = unixTimestampInSeconds * Math.pow(2, 128) + (timeInUint256);
+  }
 
   const deployContract = async () => {
     const signer = await provider.getSigner();
     const EventTicketingABI = eventTicketingArtifact.abi;
     const EventTicketingBytecode = eventTicketingArtifact.bytecode;
     
-
     const EventTicketingFactory = new ethers.ContractFactory(EventTicketingABI, EventTicketingBytecode, signer);
     
+    convertDateTime();
 
     const eventTicketing = await EventTicketingFactory.deploy(
       maxTickets, 
       eventLocation, 
       eventName, 
-      eventTime
+      BigInt(eventDateTimeInUint256)
     );
 
     setContractAddress(eventTicketing.target);
@@ -38,6 +50,7 @@ const App = () => {
   
 
   return (
+    
     <div>
       <div className='header'>BCAMP 2023 - Event Ticketing Dapp</div>
       <p>Please enter your event data by filling out the fields below:</p>
@@ -45,7 +58,8 @@ const App = () => {
         <input value={maxTickets} onChange={(e) => setMaxTickets(e.target.value)} placeholder="Max Tickets" />
         <input value={eventLocation} onChange={(e) => setEventLocation(e.target.value)} placeholder="Event Location" />
         <input value={eventName} onChange={(e) => setEventName(e.target.value)} placeholder="Event Name" />
-        <input value={eventTime} onChange={(e) => setEventTime(e.target.value)} placeholder="Event Time (eg. 1703484000)" />
+        <input value={eventDate} onChange={(e) => setEventDate(e.target.value)} placeholder="Event Date (2023-07-10)" />
+        <input value={eventTime} onChange={(e) => setEventTime(e.target.value)} placeholder="Event Time (12:00)" />
         <button className='deploy-button' onClick={deployContract}>Create Event</button>
         <label value={contractAddress}></label>
       </div>
