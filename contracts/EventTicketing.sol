46171c218a4cce27ed5c7e3986c9530bbc0e31cd
Travis Richardson <tlrichar@gmail.com>
2023-07-16 17:20:32 -0400

    tests and contract changes for buy and batch buy

--- contracts/EventTicketing.sol
+++ contracts/EventTicketing.sol
@@ -2,14 +2,16 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol"; //added this
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "hardhat/console.sol";
 
 /// @custom:security-contact @Bigbadbeard
-contract EventTicketing is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, AccessControl {
+ contract EventTicketing is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, AccessControl, IERC721Receiver {
     using Counters for Counters.Counter;
 
     Counters.Counter private _seatIdCounter;
@@ -57,13 +59,19 @@ contract EventTicketing is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, 
         eventLocation = _eventLocation;
         eventTime = _eventTime;
         eventName = _eventName;
-        cost = 100;
+        cost = uint256(100);
 
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
+    
+    
 
     function _baseURI() internal pure override returns (string memory) {
         return "http://www.tobedetermined.com";
+    }
+
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) external override returns (bytes4) {
+        return this.onERC721Received.selector;
     }
 
     function safeMint() public onlyOwner {
@@ -74,7 +82,6 @@ contract EventTicketing is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, 
     }
 
   
-
      function batchMint(uint256 mintQuantity) public onlyOwner {
         uint256 seatId = _seatIdCounter.current();
         require(seatId + mintQuantity <= maxTickets, "Max number of tickets already minted or batch will be too large");
@@ -82,6 +89,7 @@ contract EventTicketing is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, 
         for(uint256 i = 0; i < mintQuantity; i++){
             _safeMint(address(this), seatId);
             seatId++;
+            _seatIdCounter.increment();
         } 
     }
 
@@ -118,6 +126,10 @@ contract EventTicketing is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, 
         // Store the new ticket in the tickets mapping
         tickets[_ticketId] = newTicket;
 
+        //console.log("The Ticket is: %s", newTicket);
+        
+
+
         emit TicketCreated(_ticketId, msg.sender);
 
     }
@@ -135,25 +147,25 @@ contract EventTicketing is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, 
         // Check that the seat number is not already in use
         require(!seatTaken(_seatNumber), "Seat already taken");
         // Check that enough minted tickets exist
-        require(_seatNumber <= _seatIdCounter.current(), "Not enough minted tickets");
+        require(ticketId <= _seatIdCounter.current(), "Not enough minted tickets");
         // Check that the ticket is valid
-        require(tickets[_seatNumber].isValid == true, "Ticket is not valid");
+        require(tickets[ticketId].isValid == true, "Ticket is not valid");
         // Check that the user has paid enough
         require(msg.value >= tickets[_seatNumber].cost, "Not enough funds sent");
 
         // Update the ticket struct in memory
         tickets[_seatNumber].purchaser = msg.sender;
 
+
+
         // Transfer the ticket to the user
         ///////////Added this///////
+        _approve(msg.sender, ticketId);
         transferFrom(address(this), msg.sender, ticketId);
         ///////////Added this/////// it will transfer from the NFT from the smart contract "address(this)" to the buyer
 
-        // Refund any overpayment
-        ///I'm not sure that we need this
-        if (msg.value > tickets[_seatNumber].cost) {
-            payable(msg.sender).transfer(msg.value - tickets[_seatNumber].cost);
-        }
+        payable(msg.sender).transfer(msg.value - tickets[_seatNumber].cost);
+        
 
         emit TicketBought(_seatNumber, msg.sender);
     }
--- test/EventTicketing.js
+++ test/EventTicketing.js
@@ -1,291 +1,199 @@
-const { expect } = require("chai");
-const {ethers} = require("hardhat");
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
 
-describe("EventTicketing", () =>{
-
+describe('EventTicketing', () => {
   // The smart contract under test
-  let eventTicketing;
+  let eventTicketing
   // The initial contract balance
-  let initialBalance;
+  let initialBalance
   // The accounts
   let owner, ticketScanner, buyer
 
   // Example Event
-  let event = [20, "bcamp.dev", "BCAMP Fall 2023", 1]
- 
+  let event = [20, 'bcamp.dev', 'BCAMP Fall 2023', 1]
+
   //Example Ticket
-  const ticketId = 1;
-  const seatNumber = 1;
-  const cost = 100;
-  const date = 1654320000; // Example timestamp
+  let ticketId = 0
+  let seatNumber = 1
+  let cost = ethers.parseUnits('1', 'ether')
+  let date = 1654320000 // Example timestamp
+
+  // Example amount of funds sent to buy a ticket
+  const funds = 2000000000000000
 
   beforeEach(async () => {
-    [owner, ticketScanner, buyer] = await ethers.getSigners();
-    eventTicketing = await ethers.deployContract("EventTicketing", event);
+    ;[owner, ticketScanner, buyer] = await ethers.getSigners()
+
+    eventTicketing = await ethers.deployContract('EventTicketing', event)
     await eventTicketing.waitForDeployment()
 
     // setup ticket scanning roles for the event
-    await eventTicketing.setupTicketScannerRoles(ticketScanner.address);
+    await eventTicketing.setupTicketScannerRoles(ticketScanner.address)
 
-  });
+    // reset gloabls
+    ticketId = 0;
+    seatNumber = 1;
+  })
 
-  it("should create tickets", async function () {
-
+  it('should create tickets', async function () {
     //test maximum tickets allowed.
     for (let i = 0; i < eventTicketing.maxTickets; i++) {
-      let tx = await eventTicketing.createTicket(ticketId, i, cost, date);
-      const ticket = await eventTicketing.tickets(ticketId);
-      expect(ticket.seatNumber).to.equal(seatNumber);
-      expect(ticket.cost).to.equal(cost);
-      expect(ticket.date).to.equal(date);
-      expect(ticket.hasBeenScanned).to.be.false;
-      expect(ticket.isValid).to.be.true;
+      let tx = await eventTicketing.createTicket(ticketId, i, cost, date)
+      const ticket = await eventTicketing.tickets(ticketId)
+      expect(ticket.seatNumber).to.equal(seatNumber)
+      expect(ticket.cost).to.equal(cost)
+      expect(ticket.date).to.equal(date)
+      expect(ticket.hasBeenScanned).to.be.false
+      expect(ticket.isValid).to.be.true
 
       // Check emitted event
-      expect(tx)
-          .to.emit(eventTicketing, "TicketCreated")
-          .withArgs(ticketId);
+      expect(tx).to.emit(eventTicketing, 'TicketCreated').withArgs(ticketId)
     }
+  })
 
-  });
-
-
-
-
-
-  it("should scan tickets", async function () {
-    await eventTicketing.safeMint();
-    await eventTicketing.createTicket(ticketId, seatNumber, cost, date);
+  it('should scan tickets', async function () {
+    await eventTicketing.safeMint()
+    await eventTicketing.createTicket(ticketId, seatNumber, cost, date)
 
     // Use connect to impersonate the ticketScanner account
-    await eventTicketing.connect(ticketScanner).scanTicket(ticketId);
+    let transaction = await eventTicketing.connect(ticketScanner).scanTicket(ticketId)
+    transaction.wait()
 
-    const ticket = await eventTicketing.tickets(ticketId);
-    expect(ticket.hasBeenScanned).to.be.true;
-
-    // Check emitted event
-    const scanEvent = await eventTicketing
-        .connect(ticketScanner)
-        .scanTicket(ticketId);
-    expect(scanEvent)
-        .to.emit(eventTicketing, "TicketScanned")
-        .withArgs(ticketId, ticketScanner.address);
-  });
-
-  it("should revoke tickets", async function () {
-    await eventTicketing.safeMint();
-    await eventTicketing.createTicket(ticketId, seatNumber, cost, date);
-
-    let ticket = await eventTicketing.tickets(ticketId);
-    expect(ticket.isValid).to.be.true;
-
-    await eventTicketing.revokeTicket(ticketId);
-    ticket = await eventTicketing.tickets(ticketId);
-    expect(ticket.isValid).to.be.false;
+    const ticket = await eventTicketing.tickets(ticketId)
+    expect(ticket.hasBeenScanned).to.be.true
 
     // Check emitted event
-    const revokeEvent = await eventTicketing.revokeTicket(ticketId);
-    expect(revokeEvent)
-        .to.emit(eventTicketing, "TicketRevoked")
-        .withArgs(ticketId);
-  });
+    const scanEvent = await eventTicketing.connect(ticketScanner).scanTicket(ticketId)
+    expect(scanEvent).to.emit(eventTicketing, 'TicketScanned').withArgs(ticketId, ticketScanner.address)
+  })
 
-  it("should mint tickets", async function () {
+  it('should revoke tickets', async function () {
+    await eventTicketing.safeMint()
+    await eventTicketing.createTicket(ticketId, seatNumber, cost, date)
 
+    let ticket = await eventTicketing.tickets(ticketId)
+    expect(ticket.isValid).to.be.true
+
+    await eventTicketing.revokeTicket(ticketId)
+    ticket = await eventTicketing.tickets(ticketId)
+    expect(ticket.isValid).to.be.false
+
+    // Check emitted event
+    const revokeEvent = await eventTicketing.revokeTicket(ticketId)
+    expect(revokeEvent).to.emit(eventTicketing, 'TicketRevoked').withArgs(ticketId)
+  })
+
+  it('should mint tickets', async function () {
     // Call the safeMint function
-    await eventTicketing.safeMint();
+    await eventTicketing.safeMint()
 
     // Verify that the NFT is recorded as minted
-    const seatId = 0; // Set the seat ID to the appropriate value
-    const ticket = eventTicketing.tickets(seatId+1);
+    const seatId = 0 // Set the seat ID to the appropriate value
+    const ticket = eventTicketing.tickets(seatId + 1)
 
     // Check can mint max tickets
     for (let i = 1; i <= eventTicketing.maxTickets; i++) {
-      await eventTicketing.safeMint();
+      await eventTicketing.safeMint()
     }
+  })
 
+  it('should mint a new ticket when called by the owner', async () => {
+    const seatId = await eventTicketing.getNumTicketsMinted()
 
-  });
+    let transaction = await eventTicketing.connect(owner).safeMint()
+    await transaction.wait()
 
+    const finalSeatId = await eventTicketing.getNumTicketsMinted()
+    expect(finalSeatId).to.equal(seatId + BigInt(1))
+  })
 
-
-  it("should mint a new ticket when called by the owner", async () => {
-    const initialSeatId = await eventTicketing.getNumTicketsMinted();
-
-    await eventTicketing.safeMint();
-
-    const finalSeatId = await eventTicketing.getNumTicketsMinted();
-    const ownerOfTicket = await eventTicketing.ownerOf(initialSeatId);
-
-    expect(finalSeatId).to.equal(initialSeatId + BigInt(1));
-    expect(ownerOfTicket).to.equal(owner.address);
-  });
-
-  it("should revert if someone other than the owner tries to mint", async () => {
+  it('should revert if someone other than the owner tries to mint', async () => {
     await expect(eventTicketing.connect(ticketScanner).safeMint()).to.be.revertedWith(
-        "Ownable: caller is not the owner"
-    );
-  });
+      'Ownable: caller is not the owner',
+    )
+  })
 
-  it("should revert if the maximum number of tickets is already minted", async () => {
+  it('should revert if the maximum number of tickets is already minted', async () => {
     // Set the maxTickets to a small value for testing purposes
-    maxTickets = event[0];
+    maxTickets = event[0]
 
     // Mint the maximum number of tickets
     for (let i = 0; i < maxTickets; i++) {
-      await eventTicketing.safeMint();
+      await eventTicketing.safeMint()
+    }
+  })
+
+  
+  it('should return true if a seat is taken', async function () {
+    // Mint and create a ticket
+    await eventTicketing.safeMint()
+    await eventTicketing.createTicket(ticketId, seatNumber, cost, date)
+
+    const transaction = await eventTicketing.connect(buyer).buyTicket(seatNumber, ticketId, { value: cost })
+    await transaction.wait()
+
+    const isTaken = await eventTicketing.seatTaken(seatNumber)
+    expect(isTaken).to.be.true
+  })
+
+
+  it('should allow for a batch of tickets to be minted, created, and bought', async function () {
+    // Mint and create a ticket
+    await eventTicketing.batchMint(3)
+
+    for (let i = 0; i < 3; i++) {
+      await eventTicketing.createTicket(ticketId, seatNumber, cost, date)
+      ticketId++
+      seatNumber++
     }
 
-  });
+    // Check ownership of the newly created tickets
+    for (let i = 0; i < 3; i++) {
+      const transaction = await eventTicketing.connect(buyer).buyTicket(i + 1, i, { value: cost })
+      await transaction.wait()
+      const owner = await eventTicketing.ownerOf(i)
+      expect(owner).to.equal(buyer.address)
 
-  it("should return false if the seat is not taken", async () => {
-    const seat = 1;
-    const isTaken = await eventTicketing.seatTaken(seat);
-    expect(isTaken).to.be.false;
-  });
-
-  it("should return true if the seat is taken", async () => {
-
-
-    // Mint and create a ticket
-    await eventTicketing.safeMint();
-    await eventTicketing.createTicket(ticketId, seatNumber, cost, date);
-    await eventTicketing.buyTicket(ticketId, {value: cost});
-
-    const isTaken = await eventTicketing.seatTaken(seatNumber);
-    expect(isTaken).to.be.true;
-  });
-
-
-  it("should create a batch of tickets", async function () {
-
-    const initialSeatId = await eventTicketing.getNumTicketsMinted();
-
-      //await expect(eventTicketing.connect(ticketScanner).batchMint(2)).to.be.revertedWith(
-      //    "Ownable: caller is not the owner"
-      //);
-
-    //expect (batchMint (0)).to.be.false
-
-    // Check just 1 batch mint
-    eventTicketing.batchMint (1);
-    
-    const ownerOfTicket = await eventTicketing.ownerOf(initialSeatId);
-    expect(ownerOfTicket).to.equal(owner.address);
-
-    const finalSeatId = await eventTicketing.getNumTicketsMinted();
-    expect(finalSeatId).to.equal(initialSeatId + BigInt(1));
-    
-
-    //batchMint (eventTicketing.maxTickets-1);
-    //batchMint (eventTicketing.maxTickets);
-    //batchMint  (eventTicketing.maxTicktes +1);
-    
-    });
-
-
-  it('should allow a user to buy a ticket', async function() {
-
-    const initialBalance = 0;
-    const initialUserBalance = 10000;
-    const gasCost = 0;
-    const buyerBalanceBefore = await ethers.provider.getBalance(buyer); //10000000000000000000000n wei
-
-    // Mint and create a ticket
-    await eventTicketing.safeMint();
-    await eventTicketing.createTicket(ticketId, seatNumber, cost, date);
-
-    // Check that a seat is marked as not taken
-    let isSeatTaken = await eventTicketing.seatTaken(seatNumber);
-    expect(isSeatTaken).to.be.false;
-
-    // Use connect to impersonate a buyer account
-    let tx = await eventTicketing.connect(buyer).buyTicket(ticketId, {value: cost});
-
-    // Check that the seat is marked as taken after they purchased it
-    isSeatTaken = await eventTicketing.seatTaken(seatNumber);
-    expect(isSeatTaken).to.be.true;
-
-    // Check that the purchaser is set correctly
-    const [, , , , , purchaser] = await eventTicketing.tickets(seatNumber);
-    expect(purchaser).to.equal(await buyer.getAddress());
-
-    // Check that the contract balance has increased by the ticket cost
-    const newBalance = await ethers.provider.getBalance(await eventTicketing.getAddress());
-    const expectedBalance = BigInt(initialBalance) + BigInt(cost);
-    expect(expectedBalance).to.equal(newBalance);
-
-    // Check that the user's balance has decreased by the ticket cost
-    const newUserBalance = await ethers.provider.getBalance(purchaser); //9999999846781235342435n wei
-    expect(newUserBalance).to.be.lessThan(buyerBalanceBefore); // this should be exact. I think gas is messing it up maybe so need to add in accounting for gas.
-
-    // Check emitted event
-    expect(tx)
-        .to.emit(eventTicketing, "TicketBought")
-        .withArgs(ticketId);
-
-  });
-
-  it('should withdraw contract balance', async function () {
-
-    // Populate the contract with some funds:
-    await eventTicketing.safeMint();
-
-    await eventTicketing.createTicket(ticketId, seatNumber, cost, date);
-    await eventTicketing.connect(buyer).buyTicket(ticketId, {value: cost});
-
-    initialBalance = await ethers.provider.getBalance(await eventTicketing.getAddress());
-    initialOwnerBalance = await ethers.provider.getBalance(owner.address);
-
-    // Assert that the contract balance has the correct amount
-    expect(initialBalance).to.equal(cost);
-
-    // Call the withdraw function
-    const tx = await eventTicketing.withdraw();
-
-    // calculate gas (save for later)
-    const receipt = await tx.wait();
-    const gasUsed = receipt.gasUsed;
-    expect(gasUsed).to.exist;
-
-    // Check the balance of the contract after withdrawal
-    const contractBalance = await ethers.provider.getBalance(await eventTicketing.getAddress());
-
-    // Assert that the contract balance is zero after withdrawal
-    expect(contractBalance).to.equal(0);
-
-    // Check the balance of the owner account
-    const newOwnerBalance = await ethers.provider.getBalance(owner.address);
-
-    // Assert that the owner received the correct balance
-    //expect(newOwnerBalance).to.be.greaterThan(BigInt(initialOwnerBalance) ); // need to account for gas. not sure how just yet. For now just check its grown in size
-  });
-
-
-  it('should make sure ticket Validity works', async function () {
-
-    // Mint and create a ticket
-    await eventTicketing.safeMint();
-    await eventTicketing.createTicket(ticketId, seatNumber, cost, date);
-    await eventTicketing.buyTicket(ticketId, {value: cost});
-
-    // Check that the ticket is valid
-    let ticket = await eventTicketing.tickets(ticketId);
-    expect(ticket.isValid).to.be.true;
+      // Check that the ticket is valid
+    let ticket = await eventTicketing.tickets(i)
+    expect(ticket.isValid).to.be.true
 
     // Revoke the ticket
-    await eventTicketing.revokeTicket(ticketId);
+    await eventTicketing.revokeTicket(i)
 
     // Check that the ticket is no longer valid
-    ticket = await eventTicketing.tickets(ticketId);
-    expect(ticket.isValid).to.be.false;
+    ticket = await eventTicketing.tickets(i)
+    expect(ticket.isValid).to.be.false
 
     // Check emitted event
-    const revokeEvent = await eventTicketing.revokeTicket(ticketId);
-    expect(revokeEvent)
-        .to.emit(eventTicketing, "TicketRevoked")
-        .withArgs(ticketId);
+    const revokeEvent = await eventTicketing.revokeTicket(i)
+    expect(revokeEvent).to.emit(eventTicketing, 'TicketRevoked').withArgs(i)
 
-  });
+    }
+  })
 
-});
+  it('should withdraw contract balance', async function () {
+    // Mint and create a ticket
+    await eventTicketing.safeMint()
+    await eventTicketing.createTicket(ticketId, seatNumber, cost, date)
+
+    const transaction = await eventTicketing.connect(buyer).buyTicket(seatNumber, ticketId, { value: cost })
+    await transaction.wait()
+
+    const isTaken = await eventTicketing.seatTaken(seatNumber)
+    expect(isTaken).to.be.true
+
+   initialBalance = await ethers.provider.getBalance(await eventTicketing.getAddress())
+   initialOwnerBalance = await ethers.provider.getBalance(owner.address)
+
+   // Call the withdraw function
+   const tx = await eventTicketing.withdraw()
+
+   // Check the balance of the contract after withdrawal
+   const contractBalance = await ethers.provider.getBalance(await eventTicketing.getAddress())
+
+   // Assert that the contract balance is zero after withdrawal
+   expect(contractBalance).to.equal(BigInt(0))
+
+ })
+})
